da8b9143fd0544c1d7ae059f4317d1c4bc9c8f51
DESKTOP-H184TC7\Vo Huyen Lan Uyen <lanuyen.uit@gmail.com>
2018-05-21 19:47:06 +0700

    fix winner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import _ from 'lodash';?
+import _ from 'lodash';
 import './App.css';
 
 class App extends Component {
@@ -11,9 +11,11 @@ class App extends Component {
         [0, 0, 0],
         [0, 0, 0]
       ],
-      resultArr1: [[]],
-      resultArr2: [[]],
       x_flag: false,
+      winer: null,
+      countWiner: false
+
+
     }
   }
 
@@ -33,12 +35,30 @@ class App extends Component {
   }
 
   handleClick = (row,col) => {
-    let {x_flag,arr} = this.state;
+    let {x_flag,arr, winer} = this.state;
     if(arr[row][col] !== 0) {return}
-    arr[row][col] = x_flag ? 1 : 2;
+
+    if (winer) {
+      arr[row][col] = 0
+    }
+    else {
+      arr[row][col] = x_flag ? 1 : 2;
+    }
+
     this.setState({
       x_flag: !x_flag,
       arr: arr
+    })
+
+    if (this.checkWinByRow(arr[row][col], row, col) 
+    || this.checkWinByCol(arr[row][col], row, col)
+    || this.checkWinByMainCross(arr[row][col], row, col)
+    || this.checkWinBySubCross(arr[row][col], row, col) ) {
+      winer = arr[row][col] === 1 ? 'X ': 'O '
+    }
+    this.setState({
+      winer: winer,
+      countWiner: !winer
     })
   }
 
@@ -50,8 +70,6 @@ class App extends Component {
       let rendertd = function(arr){
         return arr.map((val, j) =>{return _this.renderValue(val, i, j)})
       };
-
-
       return (<tr key={`row ${i}`}> {rendertd(subarr)}</tr>)
     })
   }
@@ -62,17 +80,99 @@ class App extends Component {
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
-      ]
+      ],
+      winer: null
     })
   }
+
+  checkWinByRow = (_value, i, j) => {
+    let {arr} = this.state
+    let result = true
+    _.forEach(arr[i], (value) => {
+      if (_value !== value){
+        result = false
+      }
+    })
+    return result
+  }
+
+  checkWinByCol = (_value, _i, j) => {
+    let result = true
+    let {arr} = this.state
+    _.forEach([0,1,2], (i) => {
+      if (_value !== arr[i][j]){
+        result = false
+      }
+    })
+    return result
+  }
+
+  checkWinByMainCross= (_value, _i, j) => {
+    let result = true
+    let {arr} = this.state
+    _.forEach([0,1,2], (i) => {
+      if (_value !== arr[i][i]){
+        result = false
+      }
+      
+    })
+    return result
+  }
+
+  checkWinBySubCross= (_value, _i, j) => {
+    let result = true
+    let {arr} = this.state
+    _.forEach([0,1,2], (i) => {
+      if (_value !== arr[i][2-i]){
+        result = false
+      }
+    })
+    return result
+  }
+
+  componentDidUpdate() {
+    let {winer} = this.state
+
+    if (winer) {
+
+      // alert(winer + 'win')
+      // window.setTimeout(()=>{alert (winer + 'win')}, 0)
+      console.log (winer + 'win')
+
+    }
+
+  }
+
+  executeX = () => {
+    this.setState({
+      x_flag: true
+    })
+  }
+
+  executeO = () => {
+    this.setState({
+      x_flag: false
+    })
+  }
+
+  shouldComponentUpdate() {
+    let {countWiner} = _.clone(this.state)
+
+    return !countWiner
+  }
+
+
 
   render() {
     return (
       <div>
         <button className="btn" onClick={this.handleReset}>Reset</button>
+        <button className="btn" onClick={this.executeX}>X priority</button>
+        <button className="btn" onClick={this.executeO}>O priority</button>
         <table className="table table-bordered">
           <tbody>
-          {this.renderMatrix()}
+          {
+            this.renderMatrix()}
           </tbody>
         </table>
       </div>
